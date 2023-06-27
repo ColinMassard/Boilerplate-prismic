441f03c095453a9f5d279752f3b1a1c9859627d5
ColinMassard <colinmassard@gmail.com>
2023-06-27 21:19:29 +0200

    UP(Boilerplate) Tailwindcss CSS & Page setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./views/**/*.pug', './styles/index.sass'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1B73E8'
+      }
+    }
+  },
+  plugins: []
+}
